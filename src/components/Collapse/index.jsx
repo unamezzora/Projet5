dd3a245a0f4fed639684c2cc56c2a65ee388bafa
Tatiana Zemliakova <tatzemliakova@gmail.com>
2024-06-15 23:55:12 +0200

    La finalisation du Collapse

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,26 +1,26 @@
-import { useEffect, useState, useRef } from "react";
+import { useState } from "react";
+import PropTypes from 'prop-types'
 import '../../style/collapse.scss'
-import ferme from '../../assets/ferme.svg'
 
-function Collapse () {
+function Collapse ({ collapseTitle, collapseContenu }) {
     const [isOpen, setIsOpen] = useState(false)
 
     return isOpen ? (
         <div className="collapse">
             <div className="collapse__title">
-                <h1>Fiabilité</h1>
+                <h1>{collapseTitle} </h1>
                 <button className="collapse__title__btn" onClick={() => setIsOpen(false)}>
                     <svg className="collapse__title__btn--ouvert" viewBox="-6 0 36 15">
                         <path stroke="none"  fill="#FFFFFF" viewBox="0 0 10 16" d="m13.2103,13.8522c-0.6694,0.6694 -1.7565,0.6694 -2.426,0l-10.28224,-10.28232c-0.66942,-0.66942 -0.66942,-1.75656 0,-2.42598c0.66942,-0.66942 1.75656,-0.66942 2.42598,0l9.07196,9.072l9.072,-9.06664c0.6694,-0.66942 1.7565,-0.66942 2.4259,0c0.6695,0.66942 0.6695,1.75656 0,2.42597l-10.2822,10.28227l-0.0054,-0.0053z" />
                     </svg>     
                 </button>
             </div>
-            <p className="collapse__contenu">Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
+            <p className="collapse__contenu">{collapseContenu}</p>
         </div>
     ) : (
         <div className="collapse">
             <div className="collapse__title">
-                <h1>Fiabilité</h1>
+                <h1>{collapseTitle}</h1>
                 <button className="collapse__title__btn" onClick={() => setIsOpen(true)}>
                     <svg className="collapse__title__btn--ferme"
                     viewBox="-6 0 36 15">
@@ -31,6 +31,11 @@ function Collapse () {
         </div>
         
     )
+}
+
+Collapse.propType = {
+    collapseTitle: PropTypes.string,
+    collapseContenu: PropTypes.string,
 }
 
  export default Collapse
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,20 +1,39 @@
-import { useState } from "react";
 import imageBanner from '../../assets/imageBanner2.svg'
 import Collapse from "../../components/Collapse";
 import '../../style/about.scss'
 
+const aPropos = [
+    {
+        collapseTitle: 'Fiabilité',
+        collapseContenu: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
+    },
+    {
+        collapseTitle: 'Respect',
+        collapseContenu: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+    },
+    {
+        collapseTitle: 'Service',
+        collapseContenu: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+    },
+    {
+        collapseTitle: 'SerSécurité',
+        collapseContenu: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+]
+
 function About() {
-    const [collapse, updateCollapse] = useState(0)
     return (
         <div className="apropos">
             <img src={imageBanner} className='apropos__banner' alt="les montagnes" />
             <div className="apropos__banner--Sombre"></div>
             <div className="apropos__collapse">
-                <Collapse>
-                    <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
-                </Collapse>
-                
-         
+                {aPropos.map((valeur, index) => (
+                    <Collapse
+                        key={`${aPropos.collapseTitle}-${index}`}
+                        collapseTitle={valeur.collapseTitle}
+                        collapseContenu={valeur.collapseContenu}
+                    />
+                ))}
             </div>
         </div>
     );
