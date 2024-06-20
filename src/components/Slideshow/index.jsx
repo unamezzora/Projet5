5ce306737d76c2b4bac41e2ab86d74a2770f1739
Tatiana Zemliakova <tatzemliakova@gmail.com>
2024-06-20 11:42:31 +0200

    Modification Slideshow, la page Logement

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -1,43 +1,78 @@
 import PropTypes from 'prop-types'
 import { useEffect, useState } from 'react'
-import "../../style/slideshow.scss"
+import '../../style/slideshow.scss'
 
 function Slideshow({ data }) {
-    const [currentIndex, setCurrentIndex] = useState(0)
-    const prevPicture = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === 0 ? data.pictures.lenght - 1 : prevIndex - 1));
-    }
-    const nextPicture = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === data.pictures.lenght - 1 ? 0 : prevIndex + 1));
-    }
-
-    useEffect(() => {
-        setCurrentIndex(0)
-    }, [data])
-
-
-    return (
-        <div className='slideshow'>
-            <img src={data.pictures[currentIndex]} className='slideshow__img' alt={data.title} />
-            <button onClick={prevPicture} className='slideshow__btn slideshow__btn--prev'>
-                <svg width="96" height="120" viewBox="0 0 96 120" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M70.04 15.7831L62.92 8.70312L23.36 48.3031L62.96 87.9031L70.04 80.8231L37.52 48.3031L70.04 15.7831Z" fill="white"/>
-                </svg>
-            </button>
-            <button onClick={nextPicture} className='slideshow__btn slideshow__btn--next'>
-                <svg width="96" height="121" viewBox="0 0 96 121" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M25.96 81.3458L33.04 88.4258L72.64 48.8258L33.04 9.22583L25.96 16.3058L58.48 48.8258L25.96 81.3458Z" fill="white"/>
-                </svg>
-            </button>
-            
-            
-        </div>
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const prevPicture = () => {
+    setCurrentIndex((currentIndex) =>
+      currentIndex === 0 ? data.pictures.length - 1 : currentIndex - 1
     )
-    
+  }
+  const nextPicture = () => {
+    setCurrentIndex((currentIndex) =>
+      currentIndex === data.pictures.length - 1 ? 0 : currentIndex + 1
+    )
+  }
+
+  useEffect(() => {
+    setCurrentIndex(0)
+  }, [data])
+
+  return (
+    <div className="slideshow">
+      <img
+        src={data.pictures[currentIndex]}
+        className="slideshow__img"
+        alt={data.title}
+      />
+      {data.pictures.length > 1 ? (
+        <div>
+          <button
+            className="slideshow__btn slideshow__btn--prev"
+            onClick={prevPicture}
+          >
+            <svg
+              width="96"
+              height="120"
+              viewBox="0 0 96 120"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M70.04 15.7831L62.92 8.70312L23.36 48.3031L62.96 87.9031L70.04 80.8231L37.52 48.3031L70.04 15.7831Z"
+                fill="white"
+              />
+            </svg>
+          </button>
+          <button
+            className="slideshow__btn slideshow__btn--next"
+            onClick={nextPicture}
+          >
+            <svg
+              width="96"
+              height="121"
+              viewBox="0 0 96 121"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M25.96 81.3458L33.04 88.4258L72.64 48.8258L33.04 9.22583L25.96 16.3058L58.48 48.8258L25.96 81.3458Z"
+                fill="white"
+              />
+            </svg>
+          </button>
+          <div className="slideshow__num">
+            {currentIndex + 1} / {data.pictures.length}
+          </div>
+        </div>
+      ) : null}
+    </div>
+  )
 }
 Slideshow.propTypes = {
-        pictures: PropTypes.array.isRequired,
-        title: PropTypes.string.isRequired,
-    }
+  pictures: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
+}
 
 export default Slideshow
--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,94 +1,86 @@
-import { useEffect, useState } from "react";
-import '../../style/logement.scss';
-import { useParams } from "react-router-dom";
-import Collapse from "../../components/Collapse";
-import Slideshow from "../../components/Slideshow";
+import { useEffect, useState } from 'react'
+import '../../style/logement.scss'
+import { useParams } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+import Slideshow from '../../components/Slideshow'
 import PropTypes from 'prop-types'
 
-
 function Logement() {
-    const { id: logementId } = useParams()
-    const [logementData, setLogementData] = useState(null)
-    
-    useEffect(() => {
-        fetch('/data.json')
-            .then((response) => response.json())
-            .then((jsonResponse) => {
-                
-                
+  const { id: logementId } = useParams()
+  const [logementData, setLogementData] = useState(null)
 
-                const logement = jsonResponse.find((item) => item.id === logementId)
-                setLogementData(logement)
-                console.log(jsonResponse[0])
-            })
+  useEffect(() => {
+    fetch('/data.json')
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        const logement = jsonResponse.find((item) => item.id === logementId)
+        setLogementData(logement)
+        console.log(jsonResponse[0])
+      })
+  }, [logementId])
 
-        }, [logementId])
-
-        
-
-        return (
-            <div>
-                {logementData ? (
-                        <div className='logement'>
-                            <div className='logement__img'>
-                                <Slideshow
-                                data={logementData}
-                                />
-                            </div>
-                            <div className="logement__contenu">
-                                <div className="logement__contenu__title">
-                                    <h1>{logementData.title}</h1>
-                                    <p>{logementData.location}</p>
-                                </div>
-                                 <div className="logement__contenu__tags">
-                                 {logementData.tags}
-                                </div>
-                                <div className="logement__contenu__supp">    
-                                    {logementData.host && (
-                                        <div className="logement__contenu__supp__host">
-                                            <p>{logementData.host.name} </p>
-                                            <img src={logementData.host.picture} alt={logementData.host.name} />
-                                        </div>
-                                    )}
-                                    <div className="logement__contenu__supp__rating">
-                                    {logementData.rating}
-                                    </div>
-                                </div>
-                            </div>    
-                            <div className="logement__description">
-                                <div className="logement__description_collapse">
-                                    <Collapse 
-                                    collapseTitle="Description"
-                                    collapseContenu={logementData.description}
-                                    />
-                                </div>
-                                <div className="logement__description__collapse">
-                                    <Collapse
-                                    collapseTitle="Équipements"
-                                    collapseContenu={logementData.equipments}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-
-                ) : (
-                    <p>Loading...</p>
-                )}
+  return (
+    <div>
+      {logementData ? (
+        <div className="logement">
+          <div className="logement__img">
+            <Slideshow data={logementData} />
+          </div>
+          <div className="logement__contenu">
+            <div className="logement__contenu__title">
+              <h1>{logementData.title}</h1>
+              <p>{logementData.location}</p>
+              <div className="logement__contenu__title__tags">
+                {logementData.tags.map((tag, index) => (
+                  <div key={`${tag}-${index}`}>{tag}</div>
+                ))}
+              </div>
             </div>
-            
-            
-
-        )
+            <div className="logement__contenu__supp">
+              {logementData.host && (
+                <div className="logement__contenu__supp__host">
+                  <p>{logementData.host.name} </p>
+                  <img
+                    src={logementData.host.picture}
+                    alt={logementData.host.name}
+                  />
+                </div>
+              )}
+              <div className="logement__contenu__supp__rating">
+                {logementData.rating}
+              </div>
+            </div>
+          </div>
+          <div className="logement__description">
+            <div className="logement__description_collapse">
+              <Collapse
+                collapseTitle="Description"
+                collapseContenu={logementData.description}
+              />
+            </div>
+            <div className="logement__description__collapse">
+              <Collapse
+                collapseTitle="Équipements"
+                collapseContenu={logementData.equipments}
+              />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  )
 }
 Logement.propTypes = {
-    pictures: PropTypes.array.isRequired,
-    title: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    tags: PropTypes.string.isRequired,
-    host: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
-    description: PropTypes.string.isRequired,
-    equipments: PropTypes.string.isRequired,
+  pictures: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  host: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  equipments: PropTypes.string.isRequired,
 }
 
 export default Logement
